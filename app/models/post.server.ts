1cd01579e4cda37d29ad2fba50345f17e3f41cf6
Toby Trembath <toby@tastydigital.com>
2022-09-28 12:21:37 +0100

    Tutorial completed

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,18 +1,17 @@
 
-  type Post = {
-    slug: string;
-    title: string;
-  };
+  import { prisma } from "~/db.server";
+  
+  import type { Post } from "@prisma/client";
+  export type { Post };
 
-  export async function getPosts(): Promise<Array<Post>> {
-    return [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ];
+  export async function getPosts() {
+    return prisma.post.findMany();
+  }
+
+  export async function getPost(slug: string) {
+    return prisma.post.findUnique({ where: { slug } });
+  }
+
+  export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">) {
+    return prisma.post.create({ data: post });
   }
---
+++ prisma/migrations/20220928092747_debugging/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `likes` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `published` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `views` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "slug" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "markdown" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Post" ("createdAt", "markdown", "slug", "title", "updatedAt") SELECT "createdAt", "markdown", "slug", "title", "updatedAt" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
